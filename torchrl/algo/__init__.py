c08fab2bd25a62104bdc2ef5a8c04e1e85bd30ba
piao-0429 <huapu0429@gmail.com>
2022-07-01 17:11:38 +0800

    init

---
+++ torchrl/algo/__init__.py
@@ -0,0 +1,15 @@
+from .off_policy import *
+
+__all__ = [
+    'SAC',
+    'MTSAC',
+    'MTMHSAC',
+    'DDPG',
+    'TwinSAC',
+    'TwinSACQ',
+    'TD3',
+    'ARSAC',
+    'ARSAC_Embed',
+    'ARSAC_Embed_Net',
+    'ARSAC_v2'
+]
---
+++ torchrl/algo/off_policy/__init__.py
@@ -0,0 +1,11 @@
+from .sac import SAC
+from .mt_sac import MTSAC
+from .mtmh_sac import MTMHSAC
+from .twin_sac import TwinSAC
+from .twin_sac_q import TwinSACQ
+
+from .ar_sac import ARSAC
+from .ar_sac_v2 import ARSAC_v2
+from .ar_sac_embed import ARSAC_Embed
+from .ar_sac_net import ARSAC_Net
+from .ar_sac_embed_net import ARSAC_Embed_Net
